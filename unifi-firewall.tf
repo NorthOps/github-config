9f974235c79ecde560ac8c57530e853474083ce5
Tom Withers <tomtucka@gmail.com>
2021-03-29 19:35:53 +0000

    Don't use a broken module...

--- unifi-firewall.tf
+++ unifi-firewall.tf
@@ -1,8 +1,28 @@
-module "unifi-firewall" {
-  source      = "github.com/NorthOps/terraform-github-repository"
+resource "github_repository" "unifi_firewall" {
   name        = "terraform-unifi-firewall"
+  description = "Terraform module to manage common unifi firewall rules." 
+  auto_init   = true
 
-  delete_branch_on_merge    = true
-  description               = "Terraform module to manage common unifi firewall rules." 
-  has_issues                = true
+  allow_merge_commit     = false
+  delete_branch_on_merge = true
+  has_issues             = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_branch_protection" "unifi_firewall_main" {
+  branch         = github_repository.unifi_firewall.default_branch
+  repository     = github_repository.unifi_firewall.name
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
 }
